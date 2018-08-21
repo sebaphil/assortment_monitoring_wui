2594903c96589de0bf69a04e29ab285273f047ff
Unknown <seba.fil@live.com>
2018-08-21 12:29:19 +0200

    Files upload

---
+++ js/cors.request.js
@@ -0,0 +1,55 @@
+// Create the XHR object.
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
+
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+    // This is a sample server that supports CORS.
+    var url = 'http://localhost:3000/sub_categories';
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    // Response handlers.
+    xhr.onload = function () {
+        var text = xhr.responseText;
+        alert('Response from CORS request to ' + url + ': ' + text);
+        var jsonObject = JSON.parse(xhr.response);
+        TableFiller(jsonObject);
+    };
+
+    xhr.onerror = function () {
+        alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
+}
+
+function TableFiller(jsonListObject) {
+    var html = '<thead> <tr ><th scope = "col" > sub_category_id </th> <th scope = "col" > name </th></tr> </thead>';
+    jsonListObject.forEach(function (jsonObject) {
+        html += '<tr>';
+
+        html += '<td>' + jsonObject['sub_category_id'] + '</td>' + '<td>' + jsonObject['name'] + '</td>';
+
+        html += '</tr>';
+    })
+    document.getElementById('sub-cat-table').innerHTML = html;
+}
